aa15651706cd4dbd78d2c87e8fbb103539a0eac4
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-05 12:24:25 -0300

    Update desafio_086.py

--- desafios/desafio_086.py
+++ desafios/desafio_086.py
@@ -3,7 +3,10 @@ matriz = [[0,0,0], [0,0,0], [0,0,0]]
 
 for l in range (0,3):
     for c in range (0,3):
-        matriz [l] [c] = int(input("Digite um Valor: "))
-print(matriz)
-
-#teste
+        matriz [l] [c] = int(input(f"Digite um Valor para [{l}, {c}]: "))
+print("-=" * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f"[{matriz [l] [c]:^5}]", end="")
+    print()
+    
