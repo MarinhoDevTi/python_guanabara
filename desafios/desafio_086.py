771b912fa108ec5dfbba7d9b08630474ab99a8db
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-05 01:07:44 -0300

    Create desafio_086.py

---
+++ desafios/desafio_086.py
@@ -0,0 +1,7 @@
+#inicio do codigo...
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+
+for l in range (0,3):
+    for c in range (0,3):
+        matriz [l] [c] = int(input("Digite um Valor: "))
+print(matriz)
