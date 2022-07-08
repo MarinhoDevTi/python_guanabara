9d375736ce6a165fdd13da615b47b71fe226d7d9
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-07 21:04:26 -0300

    Update ambiente01.py

--- desafios/ambiente01.py
+++ desafios/ambiente01.py
@@ -3,6 +3,14 @@ def linha():
     print("-" * 50)
 #fim da função linha()
 
+def mensagem(msg):
+    print("-=-" * 50)
+    print(msg)
+    print("-=-" * 50)
+#fim da função mensagem(msg)
+
 linha()
 print("Meu nome é:  Diogo Marinho da silva")
 linha()
+
+mensagem("Diogo Marinho da Silva")
