899152c4d7846b8e265f2772e0ad240b3362364f
Diogo Marinho da Silva <diogomarinhomg@gmail.com>
2022-07-10 23:27:09 -0300

    Update ex001.py

---
+++ desafios/ex001.py
@@ -0,0 +1,9 @@
+#função caixa texto 01 ...
+def caixaTexto01(txto):
+    print("-=-" * 30)
+    print(txto)
+    print("-=-" * 30)
+# fim função caixa texto ...
+
+
+caixaTexto01("               ...::: Olá Seja bem vindo :::...              ")
